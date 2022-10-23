f70e351bc49263f6518ce9bc921f45cd862c6561
Edgar Mauricio Cortes <edgarm.cortes@misena.edu.co>
2022-10-23 16:25:24 -0500

    Ajustes tema1

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.curso-main-container.pb-3
+.curso-main-container.pb-3.tema1
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
     .titulo-principal.color-acento-contenido
@@ -7,19 +7,218 @@
         span 1
       h1 Control del servicio 
 
+    .fondo1.py-3
+      .row.justify-content-center.align-items-center
+        .col-lg-6.col-10
+          img.mb-lg-0.mb-0(src="@/assets/curso/tema1/img01.png")
+        .col-lg-6.col-12
+          p.mb-4 Son varias las actividades que se derivan de las funciones de un guía de turismo, entre ellas se encuentra la de definir los pasos a seguir para la identificación del nivel de satisfacción de los servicios proporcionados, de forma que los resultados puedan ser analizados y faciliten la puesta en marcha de mejoras concretas en el servicio guiado. De esta manera, es una necesidad imperiosa mantener el contacto y la comunicación relacionados con el servicio y recoger las opiniones de los usuarios al cierre de este.
+          .cajon.color-primario.p-4.mb-0
+            p.mb-0 El poder de las acciones de mejora es el fruto de la toma de decisiones que se establecen en la atención a situaciones que no son del agrado de los turistas o que no le permiten cumplir con los objetivos para lo que fuese contratado.  Definitivamente, al conocer los puntos fuertes y débiles respecto al servicio y atención que presta, facilita la calidad y la mejora continua.
+
 
     Separador
+
     #t_1_1.titulo-segundo.color-acento-contenido
       h2 1.1 Calidad en el servicio
+    
+    .fondo2.py-5
+      .row.justify-content-center.align-items-center.mb-3
+        .col-lg-6.col-10
+          img.mb-lg-0.mb-0(src="@/assets/curso/tema1/img02.svg")
+        .col-lg-6.col-12
+          p.mb-4 Para que la prestación del servicio de guianza turística se dé en términos de calidad y excelencia, se hace necesario reconocer que su enfoque es hacia el cliente, resultando fundamental que el guía de turismo comprenda cuáles son las necesidades tanto actuales como futuras del usuario, esforzándose por dar respuesta a sus expectativas.
+          p.mb-0 La calidad en el sector turístico inicia desde que el cliente solicita información del servicio a utilizar, orientándolo sobre sitios a recorrer, hoteles, servicios a prestar, tiempos de visita y de transporte. Claro está que no se puede dejar de lado la infraestructura normativa del país que ha sido promovida en este aspecto.
+    
+    p.mb-4 Algunas definiciones que se hacen necesarias para la comprensión de la calidad en el servicio son: 
+     .vineta1.d-flex.align-items-center.mb-4
+      div.p-0
+        img.mb-0(src="@/assets/curso/vineta1.svg")
+      div.px-2
+        h4.mb-0 Calidad
+        .linea(style="width: 132px")
 
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-6.col-12 
+        p.mb-4 “La calidad es el conjunto de características de una entidad, que le confiere la aptitud para satisfacer las necesidades establecidas y las implícitas” (Sellés, Pina y Navas, 2004).  
+        p.mb-4 Asimismo, la calidad hace referencia al cumplimiento o superación de las expectativas de los clientes a un costo que le represente valor. La calidad de un producto o servicio es la percepción que el cliente tiene del mismo; es una fijación mental del consumidor que asume conformidad con dicho producto o servicio y la capacidad de éste para satisfacer sus necesidades.
+        p.mb-4 Igualmente, por excelencia se entiende que un producto o servicio es de características sobresalientes. Tiene que ver con aquello que es superior a otros productos o servicios debido a sus escasas imperfecciones o debilidades. Se trata de un producto de calidad probada e innovador.
+        p.mb-lg-0.mb-0 Al relacionar los dos conceptos, se puede apreciar que están íntimamente ligados, apuntando a ir más allá en la satisfacción de las expectativas de los clientes (Servicio Nacional de Aprendizaje, 2018).
+      .col-lg-6.col-10
+        img.mb-0(src="@/assets/curso/tema1/img03.png")
+
+    .vineta1.d-flex.align-items-center.mb-4
+      div.p-0
+        img.mb-0(src="@/assets/curso/vineta1.svg")
+      div.px-2
+        h4.mb-0 Servucción
+        .linea(style="width: 132px")
+
+    .row.justify-content-center.mb-5
+      .col-lg-6.col-12.d-flex
+        div.mb-lg-0.mb-0.p-4(style="background-color: #cce7f6") 
+          img.mb-3(style="width: 53px" src="@/assets/curso/tema1/img04.svg")
+          p.mb-0 Consiste en todo lo que implica el proceso de elaboración de un servicio y la conjugación de los elementos físicos y humanos en la relación cliente – empresa, necesaria para la realización de la prestación de un servicio y cuyas características han sido determinadas desde la idea concebida, hasta la obtención del resultado como tal (el servicio).
+      .col-lg-6.col-12.d-flex
+        div.p-4(style="background-color: #cce7f6") 
+          img.mb-3(style="width: 53px" src="@/assets/curso/tema1/img04.svg")
+          p.mb-0 Comienza desde la concepción de la idea de determinado producto, hasta la obtención del resultado como tal, que es el servicio obtenido por el cliente, quien al final lo valorará y quien constituye una pieza clave e imprescindible para que el proceso se lleve a cabo, sin su presencia será imposible referirse al concepto planteado.
+    
+    p.mb-5 Al buscar la satisfacción del cliente, la servucción debe implicar varios aspectos, entre ellos, los elementos tangibles o el soporte físico, que sirven de apoyo a la prestación del servicio. Es el componente visible para el cliente, debe estar acorde con la calidad que se espera del servicio a prestar. Lo constituyen:
+    
+    .row.justify-content-center.w-100.mb-4
+      .col.px-2
+        div.p-3.py-2.pt-3.position-relative(style="background-color:#ffe6a1")
+          img.mb-0.position-absolute(style="width: 55px; top:-25px" src="@/assets/curso/tema1/img05.svg")
+          p.mb-0 El edificio.
+      .col.px-2
+        div.p-3.py-2.pt-3.position-relative(style="background-color:#ffe6a1")
+          img.mb-0.position-absolute(style="width: 55px; top:-25px" src="@/assets/curso/tema1/img06.svg")
+          p.mb-0 Accesos.
+      .col.px-2
+        div.p-3.py-2.pt-3.position-relative(style="background-color:#ffe6a1")
+          img.mb-0.position-absolute(style="width: 55px; top:-25px" src="@/assets/curso/tema1/img07.svg")
+          p.mb-0 Oficinas.
+      .col.px-2
+        div.p-3.py-2.pt-3.position-relative(style="background-color:#ffe6a1")
+          img.mb-0.position-absolute(style="width: 55px; top:-25px" src="@/assets/curso/tema1/img08.svg")
+          p.mb-0 Mobiliario.
+      .col.px-2
+        div.p-3.py-2.pt-3.position-relative(style="background-color:#ffe6a1")
+          img.mb-0.position-absolute(style="width: 55px; top:-25px" src="@/assets/curso/tema1/img09.svg")
+          p.mb-0 Decoración.
+      .col.px-2
+        div.p-3.py-2.pt-3.position-relative(style="background-color:#ffe6a1")
+          img.mb-0.position-absolute(style="width: 55px; top:-25px" src="@/assets/curso/tema1/img10.svg")
+          p.mb-0 Materias primas.
+      .col.px-2
+        div.p-3.py-2.pt-3.position-relative(style="background-color:#ffe6a1")
+          img.mb-0.position-absolute(style="width: 55px; top:-25px" src="@/assets/curso/tema1/img11.svg")
+          p.mb-0 Materiales, etc.
+    
+    .fondo3.py-4.mb-5
+      .row.justify-content-center.align-items-center.mb-0
+        .col-lg-5.col-12 
+          p.mb-4 <b>Elementos importantes en la servucción</b>
+          p.mb-4 Personal de contacto o frontline. En el proceso de la producción de un servicio enfocado en la calidad, se debe tomar en cuenta la presencia, educación, cordialidad, mística, valores, formación, honestidad, empatía, predisposición y capacidad de atención del recurso humano que preste el servicio directamente, de esto depende gran parte del éxito del proceso.
+          p.mb-lg-0.mb-0 También se deben tomar en cuenta elementos como el servicio interno, la organización, la administración; esto es, controlar el entorno organizacional y de igual manera el conocimiento de las empresas que prestan el servicio, ejemplo: hoteles con la misma categorización, restaurantes, agencias de viaje etc., lo anterior es vital para crear diferencias competitivas con el diseño de estrategias atractivas para el cliente potencial o cautivo. Asimismo, debe tomarse en cuenta el diseño e innovación que diferencien a la organización que presta el servicio de sus competidores en el mercado.
+        .col-lg-7.col-10
+          img.mb-0(src="@/assets/curso/tema1/img12.svg")
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-10.col-12    
+        .cajon.color-primario.p-4.mb-0
+            p.mb-0 El cliente no ve la servucción, la percibe, se hace necesario para verificar el éxito del proceso o no y realizar la valoración de este por parte del cliente, lo cual permitirá asumir los cambios o ajustes pertinentes y consolidar los elementos favorables (Salazar Belloso, I. 2021).
+    
+    .vineta1.d-flex.align-items-center.mb-4
+      div.p-0
+        img.mb-0(src="@/assets/curso/vineta1.svg")
+      div.px-2
+        h4.mb-0 Normativa aplicable a la calidad turística
+        .linea(style="width: 132px")
+
+    .row.justify-content-center.align-items-center.mb-5
+        .col-lg-6.col-12 
+          p.mb-4 La calidad proviene de los procesos de normalización, los cuales buscan estandarizar cualquier elemento para que sea percibido con las mismas propiedades donde se ofrezca. Los órganos que controlan y regulan esta normalización están definidos en función del ámbito que desean cubrir, así la ISO, de rango mundial, dicta normas de la Organización Internacional de Normalización. En Europa se encuentra el Comité Europeo de Normalización que publica las normas en todas estas normas en sus diferentes ámbitos tienen la característica de velar por el funcionamiento sistemático de la calidad, sin ser exigida su observancia. <b style="color:#EB6F2D">(García Santana, 2012)</b> 
+          .cajon.color-primario.p-4.mb-0
+            p.mb-0 En Colombia, el Artículo 5° de la Ley General de Turismo define Calidad Turística como “Las normas técnicas de calidad expedidas por las Unidades Sectoriales de Normalización establecidas en el artículo 69 de la Ley 300 de 1996 relacionadas con las actividades del denominado turismo de aventura y con la sostenibilidad turística, serán de obligatorio cumplimiento por parte de los prestadores de servicios turísticos, de acuerdo con la reglamentación que expida el Ministerio de Comercio, Industria y Turismo”.
+        .col-lg-6.col-10
+          img.mb-0(src="@/assets/curso/tema1/img13.png")
+
+    p.mb-4 A continuación, se muestra el sistema nacional de Calidad en el Turismo, ver figura 1:
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-10.col-12  
+        .titulo-sexto.color-acento-contenido(data-aos="fade-left")
+          p.titulo <b>Figura 1</b> <i>Calidad y turismo</i> 
+        img.mb-0(src="@/assets/curso/tema1/img14.svg")
+
+    .row.justify-content-center.align-items-center.mb-0
+      .col-lg-5.col-10 
+        img.mb-lg-0.mb-3(src="@/assets/curso/tema1/img15.png")
+      .col-lg-5.col-12 
+        p.mb-4 Las Unidades Sectoriales de Normalización USN, están conformadas por:
+        ul.lista-ul--color.mb-4
+          li.mb-0 
+            i.fas.fa-location-arrow
+            | El Ministerio de Comercio, Industria y Turismo.
+          li.mb-0 
+            i.fas.fa-location-arrow
+            | ONIC-ICONTEC.
+          li.mb-0 
+            i.fas.fa-location-arrow
+            | Los empresarios.
+          li.mb-0 
+            i.fas.fa-location-arrow
+            | Especialistas.
+          li.mb-0 
+            i.fas.fa-location-arrow
+            | Academia.
+          li.mb-0 
+            i.fas.fa-location-arrow
+            | Usuarios.
+          li.mb-0 
+            i.fas.fa-location-arrow
+            | Gremios.
+        p.mb-0 Estas unidades elaboran los documentos normativos que definen requisitos mínimos de calidad y sostenibilidad en la prestación de los servicios turísticos. 
     Separador
     #t_1_2.titulo-segundo.color-acento-contenido
       h2 1.2   La gestión y aseguramiento de estándares
+    
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-6.col-10 
+        img.mb-lg-0.mb-3(src="@/assets/curso/tema1/img16.png")
+      .col-lg-6.col-12 
+        p.mb-4 Debido a las dinámicas y tendencias características del sector turismo, las condiciones en la prestación de los servicios turísticos y la gestión de los destinos, demandan políticas y acciones que permitan mejorar su calidad y, es precisamente bajo ese contexto que Colombia implementa la Política de Calidad Turística, con el objetivo de determinar cuáles serán los ejes estratégicos de política pública que permitirán fortalecer las ventajas competitivas del país y fomentar los procesos de difusión e implementación de las normas técnicas sectoriales y de las certificaciones de calidad turística en los prestadores de servicios turísticos.
+        .cajon.color-primario.p-4.mb-0
+          p.mb-3 Todo este esfuerzo ha permitido al país establecer a través del Ministerio de Comercio, Industria y Turismo, una serie de estándares que permiten el aseguramiento de la calidad en la prestación de los servicios turísticos, así se instauró el premio anual Nacional a la Calidad Turística y se cuentan con listados de:
+          ul.lista-ul--color.mb-0
+            li.mb-2
+              i.fas.fa-location-arrow
+              | Tarjetas de los guías profesionales.
+            li.mb-2
+              i.fas.fa-location-arrow
+              | Prestadores de servicios turísticos certificados.
+            li.mb-0
+              i.fas.fa-location-arrow
+              | Destinos y playas turísticas certificadas.
+
+    p.mb-0 Para conocer la normativa vigente se sugiere consultar en el material complementario la información suministrada por el ente de control encargado.
     
     Separador
     #t_1_3.titulo-segundo.color-acento-contenido
       h2 1.3  Sistema de gestión
 
+    .bloque-texto-g.color-primario.p-3.p-sm-4.p-md-5.mb-5
+      .bloque-texto-g__img(
+        :style="{'background-image': `url(${require('@/assets/curso/tema1/img17.png')})`}"
+      )
+      .bloque-texto-g__texto.p-4
+        p.mb-0 Un sistema de gestión de calidad es una herramienta que le permite a cualquier organización planear, ejecutar y controlar las actividades necesarias para el desarrollo de su misión a través de productos o servicios con altos estándares de calidad, los cuales son medidos a través de indicadores de satisfacción.<br><br>Según la NTC ISO 9001:2015, la adopción de un sistema de gestión de la calidad es una decisión estratégica para una organización que le aporta el mejoramiento en su desempeño global y proporciona una base sólida para las iniciativas de desarrollo sostenible (Mejía, L. 2019).
+
+    p.mb-4 Como ejemplos se pueden citar:
+
+    .row.justify-content-center.mb-0
+      .col-lg-3.col-10.d-flex
+        .tarjeta1.w-100.py-3.px-3
+          div.d-flex.justify-content-center
+            img.mb-3(style="width:113px" src="@/assets/curso/tema1/img18.svg")
+          p.mb-0 <b>1.</b> La Ley General de Turismo (Ley 300 de 1996 y sus modificaciones).
+      .col-lg-3.col-10.d-flex
+        .tarjeta1.w-100.py-3.px-3
+          div.d-flex.justify-content-center
+            img.mb-3(style="width:113px" src="@/assets/curso/tema1/img19.svg")
+          p.mb-0 <b>2.</b> Las Normas Técnicas Sectoriales para Guías de Turismo (NTS-GT).
+      .col-lg-3.col-10.d-flex
+        .tarjeta1.w-100.py-3.px-3
+          div.d-flex.justify-content-center
+            img.mb-3(style="width:113px" src="@/assets/curso/tema1/img20.svg")
+          p.mb-0 <b>3.</b> El Decreto 1053 de julio 19 de 2020 del Ministerio de Comercio, Industria y Turismo, el cual reglamenta el guionaje turístico y su ejercicio.
+      .col-lg-3.col-10.d-flex
+        .tarjeta1.w-100.py-3.px-3
+          div.d-flex.justify-content-center
+            img.mb-3(style="width:113px" src="@/assets/curso/tema1/img21.svg")
+          p.mb-0 <b>4.</b> El Viceministerio de Turismo asumió las funciones del Consejo Profesional de Guías de Turismo.
 </template>
 
 <script>
