19b4aeba14286affcfad56a47484e6b4d9049df1
Edgar Mauricio Cortes <edgarm.cortes@misena.edu.co>
2022-10-23 20:02:02 -0500

    Ajustes tema 2

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -7,10 +7,275 @@
         span 2
       h1 Evaluación del servicio
 
+    .fondo1.py-3.mb-4
+      .row.justify-content-center.align-items-center      
+        .col-lg-6.col-12
+          p.mb-4 El concepto evaluación está relacionado con la estimación de comportamientos, eventos, conocimientos y características que pueden llegar a ser medidas, a partir de la asignación de valores numéricos y cuya función es la de comparar los resultados para determinar ajustes a los planes de acción.
+          p.mb-4 La evaluación tiene por objeto evidenciar situaciones que se presentan en el desarrollo de la prestación del servicio por parte del guía y que puede ser percibida de manera diferente por el cliente; puede revelar una posible no conformidad o una acción de mejora.
+          .cajon.color-primario.p-4.mb-lg-0.mb-3
+            p.mb-0 El guía es un prestador de servicios turísticos y su labor es susceptible de ser evaluada por quienes viven la experiencia turística. Las percepciones que se reciben en un instrumento de evaluación le permiten al guía de turismo encontrar los aciertos y desaciertos del servicio que prestó.
+        .col-lg-6.col-10
+          img.mb-0(src="@/assets/curso/tema2/img01.png")
 
+    p.mb-4 Para poder desarrollar procesos de medición, es importante tener en cuenta los siguientes pasos:
+
+    .row.justify-content-center.align-items-center.mb-4
+      .col-lg-6.col-12
+        .tarjeta1.p-3.mb-lg-0.mb-3
+          .d-flex.align-items-center.mb-0
+            div.p-0
+              img.mb-0(style="min-width:111px" src="@/assets/curso/tema2/img02.svg")
+            div.px-2
+              p.mb-0 <b>1. Definir qué es lo que se quiere medir:</b> En este caso, se puede medir el impacto del recorrido, el nivel de satisfacción percibido por el turista durante la prestación del servicio o los conocimientos del guía.
+      .col-lg-6.col-12
+        .tarjeta1.p-3.mb-0
+          .d-flex.align-items-center.mb-0
+            div.p-0
+              img.mb-0(style="min-width:111px" src="@/assets/curso/tema2/img03.svg")
+            div.px-2
+              p.mb-0 <b>2. Identificar a quién se va a evaluar:</b>  Por lo general en guianza turística se mide el desempeño del guía de turismo, el nivel de impacto del recorrido y los servicios relacionados con lo realizado por el guía.
+
+    .row.justify-content-center.align-items-center.mb-4
+      .col-lg-6.col-12
+        .tarjeta1.p-3.mb-lg-0.mb-3
+          .d-flex.align-items-center.mb-0
+            div.p-0
+              img.mb-0(style="min-width:111px" src="@/assets/curso/tema2/img04.svg")
+            div.px-2
+              p.mb-0 <b>3. Seleccionar el instrumento más adecuado.</b>
+      .col-lg-6.col-12
+        .tarjeta1.p-3.mb-0
+          .d-flex.align-items-center.mb-0
+            div.p-0
+              img.mb-0(style="min-width:111px" src="@/assets/curso/tema2/img05.svg")
+            div.px-2
+              p.mb-0 <b>4. Diseñar el instrumento:</b>  Debe contener información cualitativa y cuantitativa.
+
+    .row.justify-content-center.align-items-center.mb-4
+      .col-lg-6.col-12
+        .tarjeta1.p-3.mb-lg-0.mb-3
+          .d-flex.align-items-center.mb-0
+            div.p-0
+              img.mb-0(style="min-width:111px" src="@/assets/curso/tema2/img06.svg")
+            div.px-2
+              p.mb-0 <b>5. Aplicación del instrumento:</b>  Puede ser virtual o físico.
+      .col-lg-6.col-12
+        .tarjeta1.p-3.mb-0
+          .d-flex.align-items-center.mb-0
+            div.p-0
+              img.mb-0(style="min-width:111px" src="@/assets/curso/tema2/img07.svg")
+            div.px-2
+              p.mb-0 <b>6. Análisis de la información y elaboración de un informe.</b>
+
+    p.mb-5 El servicio de guianza turística es una actividad que es susceptible de medición.  Realizar esta actividad, permite a los guías y a las agencias que los contratan, analizar las percepciones de los clientes frente a la prestación del servicio.
+    p.mb-4 Los objetivos de la evaluación están definidos por:
+
+    .row.justify-content-center.mb-5
+      .col-lg-3.col-10.d-flex
+        .tarjeta2.w-100.p-4
+          div.d-flex.justify-content-center
+            img.mb-3(style="width:128px" src="@/assets/curso/tema2/img08.png")
+          p.mb-0.text-center Conocer las percepciones de los turistas que vivenciaron el servicio.
+      .col-lg-3.col-10.d-flex
+        .tarjeta2.w-100.p-4
+          div.d-flex.justify-content-center
+            img.mb-3(style="width:128px" src="@/assets/curso/tema2/img09.png")
+          p.mb-0.text-center Medir la calidad de los servicios prestados en un recorrido.
+      .col-lg-3.col-10.d-flex
+        .tarjeta2.w-100.p-4
+          div.d-flex.justify-content-center
+            img.mb-3(style="width:128px" src="@/assets/curso/tema2/img10.png")
+          p.mb-0.text-center Analizar las situaciones que incidieron en la prestación del servicio. 
+      .col-lg-3.col-10.d-flex
+        .tarjeta2.w-100.p-4
+          div.d-flex.justify-content-center
+            img.mb-3(style="width:128px" src="@/assets/curso/tema2/img11.png")
+          p.mb-0.text-center Recoger información de manera cualitativa que contribuya al fortalecimiento del servicio.
+
+    .row.justify-content-center.align-items-center.fondo2.py-5.mb-0
+      .col-lg-4.col-10
+        img.mb-lg-0.mb-3(src="@/assets/curso/tema2/img12.svg")
+      .col-lg-7.col-10
+        p.mb-4 El propósito es evaluar los servicios turísticos, porque permite medir la satisfacción de turistas y visitantes, gracias a los datos suministrados por los usuarios, a través de los diversos medios de recolección de información acerca del servicio recibido.
+        p.mb-0 El análisis de esta información conlleva a la generación de estrategias para un mejor aprovechamiento de los recursos de una organización, también a implementar procesos que conduzcan a la mejora del servicio y fortalecer la toma de decisiones que proyecten un servicio turístico agradable, ameno y de calidad para el usuario (Servicio Nacional de Aprendizaje, 2018).
     Separador
     #t_2_1.titulo-segundo.color-acento-contenido
       h2 2.1 Instrumentos de evaluación
+
+    .fondo3.py-3.mb-5
+      .row.justify-content-center.align-items-center  
+        .col-lg-6.col-10
+          img.mb-lg-0.mb-3(src="@/assets/curso/tema2/img13.png")    
+        .col-lg-6.col-12
+          p.mb-4 Los instrumentos de evaluación para la evaluación satisfacción del cliente, ayudan a medir con precisión qué tan contentos y satisfechos se encuentran los consumidores utilizando varios instrumentos. El equipo encargado puede diseñar instrumentos y utilizar escalas de valoración que por recomendación técnica son impares de tres, cinco, siete y once puntos de valoración.          
+          .cajon.color-primario.p-4.mb-0
+            p.mb-0 Los tipos de instrumentos de evaluación del servicio de la guianza turística se usan permanente partiendo del punto de vista de los clientes, prestando atención a sus percepciones, con el fin de trazar estrategias sobre cómo hacer realimentación en el servicio, utilizando diferentes formas de evaluar los servicios para poder comprender, y tener detalle de las necesidades de los turistas a la hora de tomar los servicios y que estos se ajusten a sus necesidades e intereses. 
+        
+    p.mb-5 A continuación, se describen las estrategias de evaluación: 
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-6.col-10
+        AcordionA.mb-0(tipo="a" clase-tarjeta="tarjeta tarjeta--gris")
+          div(titulo="Buzón de sugerencias")
+            p.mb-0 Este espacio de clientes externos permite de manera escrita, exponer las sugerencias, quejas y reclamos respecto de los servicios recibidos por parte de los prestadores turísticos, se indica por procedimiento, revisión periódica y un encargado que elabore el informe.  Debe darse atención a la solución de estas deducciones que se obtienen del cliente.            
+          div(titulo="Quejas y reclamos")
+            p.mb-4 Las quejas son una fuente de información importante para conocer lo que los clientes desean obtener y no lo están teniendo.
+            p.mb-4 Las empresas deben dar prelación a lo que opinan sus clientes insatisfechos, para realizar mecanismos de mejora.  También es de importancia, incentivar a sus clientes a que presenten quejas, para darle solución inmediata a las inconformidades y asegurar la confiabilidad y retención de los clientes.
+            p.mb-0 Las quejas y sugerencias establecen un aporte de ideas para realizar las mejoras en la calidad del servicio.
+          
+      .col-lg-6.col-12
+        img.mb-0(src="@/assets/curso/tema2/img14.svg")    
+
+    p.mb-5 La evaluación del servicio al cliente es uno de los datos en calidad de mayor importancia, el uso de escalas de valoración, como una forma de hacer objetiva la verificación del servicio, es usada frecuentemente en aras de tratar la información con la mayor confiabilidad del caso y pueden usarse cuantitativa o cualitativa, de acuerdo con el propósito de esta.  
+    
+    .row.justify-content-center.mb-5
+      .col-lg-5.col-12.d-flex.p-0
+        div.w-100.p-5(style="background: #3f6a30")
+          h4.mb-4.text-white Las escalas de valoración pueden ser: 
+          ul.lista-ul--color.mb-0
+            li.mb-1
+              i.fas.fa-location-arrow
+              .p-0.text-white Descriptivas.
+            li.mb-1
+              i.fas.fa-location-arrow
+              .p-0.text-white Gráficas.
+            li.mb-1
+              i.fas.fa-location-arrow
+              .p-0.text-white Numéricas.
+      .col-lg-5.col-12.d-flex.p-0
+        div.w-100.p-5(style="background: #ffd667")
+          h4.mb-4 De acuerdo con unidades de medición como frecuencia, intensidad, calidad, y otras, son diseñadas para determinar la progresión de:
+          ul.lista-ul--color.mb-0
+            li.mb-1
+              i.fas.fa-location-arrow
+              | Acciones.
+            li.mb-1
+              i.fas.fa-location-arrow
+              | Procesos.
+            li.mb-1
+              i.fas.fa-location-arrow
+              | Habilidades.
+            li.mb-1
+              i.fas.fa-location-arrow
+              | Conocimientos.
+            li.mb-1
+              i.fas.fa-location-arrow
+              | Actitudes.
+    
+    .row.justify-content-center.mb-5
+      .col-lg-10.col-12
+        .cajon.color-primario.p-4.mb-5
+            p.mb-0 La información analizada se traduce de forma comparativa y ofrece específicamente un resultado para analizarse.  Se pueden usar en diferentes instrumentos adaptando las escalas de acuerdo con el interés a evaluar.
+        
+        p.mb-0 Y la diferencia entre las escalas cuantitativas y cualitativas es:
+
+    .row.justify-content-center.mb-5
+      .col-lg-5.col-12
+        .tarjeta-avatar
+          img(style="width: 214px;top:-28px" src="@/assets/curso/tema2/img15.png")   
+          .tarjeta.color-acento-botones.p-5(style="background:#cce7f6")
+            p.mb-0.pt-4(style="color:#12263F") <br><br><br>La escala cualitativa o descriptiva, permite utilizar descripciones para identificar cada uno de los puntos o categorías de la escala en relación con los criterios. Se usan cuando se requiere saber con detalle del rasgo o atributo a observar en casos de autogestión del individuo a ser evaluado, por ejemplo, aprendizaje verificando construcción de mapas mentales, autorregulación y autoevaluación.
+      .col-lg-5.col-12
+        .tarjeta-avatar
+          img(style="width: 214px;top:-28px" src="@/assets/curso/tema2/img16.png")   
+          .tarjeta.color-acento-botones.p-5(style="background:#cce7f6")
+            p.mb-0.pt-4(style="color:#12263F") <br><br><br>La escala de calificación numérica o cuantitativa es la más utilizada, en ella se atribuyen valores numéricos al grado o nivel de calidad a lo que se va a evaluar y que evidencia en la ejecución de un criterio de evaluación dado. La escala considera un valor numérico para cada uno de los grados que la componen.
+    
+    p.mb-4 Las escalas se puntean y organizan con resultados impares; existen diferentes escalas de valoraciones: 
+    .fondo4.p-4.mb-5      
+      SlyderA(tipo="b")
+        .row.justify-content-center.mb-0.pt-4
+          .col-lg-4.col-10
+            h4.mb-5 Escala de valoración de 3 puntos
+            p.mb-lg-0.mb-3 Se utiliza cuando hay una limitación en el espacio visual, es corta y no cansa la visión.  Captura datos relevantes de los encuestados y es visualmente fácil de manejar.
+          .col-lg-7.col-12                  
+            img.mb-0(src="@/assets/curso/tema2/img17.png")  
+        .row.justify-content-center.mb-0.pt-4
+          .col-lg-4.col-10
+            h4.mb-5 Escala de valoración de 5 puntos
+            p.mb-lg-0.mb-3 Es el preferida por los evaluadores por la fiabilidad, puesto que, da resultados con las escalas unipolares tipo Likert, es decir que permite el enfoque en un solo ítem que identifica la ausencia o presencia de lo que se desea evaluar.
+          .col-lg-7.col-12                  
+            img.mb-0(src="@/assets/curso/tema2/img18.png")  
+        .row.justify-content-center.mb-0.pt-4
+          .col-lg-4.col-10
+            h4.mb-5 Escala de valoración de 7 puntos
+            p.mb-4 Funciona mejor con las escalas bipolares la cual presenta dos extremos de opinión que pueden ser de igual sólo que son de extremos diferentes. Esta escala es ideal en caso de que se mida la satisfacción al cliente para un producto o servicio lanzado recientemente. 
+            p.mb-4 Ejemplo:
+            .row.mb-lg-0.mb-3
+              .col
+                p.mb-0 Mucho        
+              .col
+                p.mb-0 Poco        
+              .col
+                p.mb-0 Neutro        
+              .col
+                p.mb-0 Poco        
+              .col
+                p.mb-0 Mucho
+            
+          .col-lg-7.col-12                  
+            img.mb-0(src="@/assets/curso/tema2/img19.png")  
+        .row.justify-content-center.mb-0.pt-4
+          .col-lg-4.col-10
+            h4.mb-5 Escala de valoración de 10 puntos
+            p.mb-lg-0.mb-3 Funciona mejor cuando la intención es reunir datos granulares, es decir, representación del nivel de detalle al que se desea almacenar la información sobre los datos a analizar. En esta escala, 9 y 10 son indicativos de la satisfacción total del cliente. Con esta herramienta se distinguen y se analizan los pros y los contras, entre las dos calificaciones para perfeccionar los procesos.
+          .col-lg-7.col-12                  
+            img.mb-0(src="@/assets/curso/tema2/img20.png")   
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-5.col-10
+        p.mb-4 Para realizar la escala de valoración se hace necesario establecer los indicadores, los cuales son:
+        ul.lista-ul--color.mb-0
+          li.mb-1
+            i.fas.fa-location-arrow
+            .p-0 Herramienta que muestra indicios o señales de una situación, actividad o resultado.
+          li.mb-1
+            i.fas.fa-location-arrow
+            .p-0 Expresión cuantitativa observable y verificable que permite describir características, comportamientos o fenómenos de la realidad lo cual se logra a través de la medición de una variable o una relación entre variables.
+      .col-lg-7.col-12
+        img.mb-0(src="@/assets/curso/tema2/img21.png")
+
+    .bloque-texto-g.color-primario.p-3.p-sm-4.p-md-5.mb-5
+      .bloque-texto-g__img(
+        :style="{'background-image': `url(${require('@/assets/curso/tema2/img22.png')})`}"
+      )
+      .bloque-texto-g__texto.p-4
+        div.p-0
+          p.mb-4 Para toda gestión de las organizaciones, los indicadores tienen como función monitorear y determinar aspectos puntuales de los diferentes procesos y procedimientos que se llevan a cabo y que además son críticos o importantes para el buen desempeño de las operaciones.<br><br>Los indicadores permiten llevar a cabo un proceso de seguimiento y facilitan la toma de decisiones, además permiten conocer la calidad de los productos o servicios que se comercializan en un mercado. La mejor forma de hacerlo es preguntando a los clientes sobre su nivel de satisfacción (Consejo Nacional de Evaluación de la Política de Desarrollo Social, 2013) el cual se encuentra en la sección de material complementario.<br><br>Los indicadores son:
+          <br>
+          ul.lista-ul--color.mb-0
+            li.mb-1
+              i.fas.fa-location-arrow
+              .p-0 Medibles.
+            li.mb-1
+              i.fas.fa-location-arrow
+              .p-0 Comprensibles, comprendidos rápidamente en su redacción. 
+            li.mb-1
+              i.fas.fa-location-arrow
+              .p-0 Controlables.  
+
+    p.mb-5 Los tipos de indicadores están relacionados con la posibilidad de contar con un referente significativo que determina a qué grado o nivel se está cerca a la meta.  Dependiendo de su aplicación o uso, pueden ser:
+
+    .row.justify-content-center.mb-0
+      .col-lg-4.col-10.d-flex
+        .tarjeta3.w-100.py-3.px-3
+          div.d-flex.justify-content-center
+            img.mb-3(style="width:130px" src="@/assets/curso/tema2/img23.svg")
+          p.mb-4 <b>1. Indicadores de gestión</b>
+          p.mb-lg-0.mb-3 Hacen referencia a la medición de aspectos relacionados con la gestión de condiciones y los recursos necesarios para el desarrollo de las actividades contratadas.
+      .col-lg-4.col-10.d-flex
+        .tarjeta3.w-100.py-3.px-3
+          div.d-flex.justify-content-center
+            img.mb-3(style="width:130px" src="@/assets/curso/tema2/img24.svg")
+          p.mb-4 <b>2. Indicadores de producto</b>
+          p.mb-lg-0.mb-3 Hacen referencia al impacto de la actividad, número de actividades, clase o tipos de actividades, número de personas atendidas, entre otras. 
+      .col-lg-4.col-10.d-flex
+        .tarjeta3.w-100.py-3.px-3
+          div.d-flex.justify-content-center
+            img.mb-3(style="width:130px" src="@/assets/curso/tema2/img25.svg")
+          p.mb-4 <b>3. Indicadores de resultado</b>
+          p.mb-lg-0.mb-3 Medición del impacto en el individuo, cambio de percepción, elementos nuevos de conocimiento, gratitud y satisfacción. 
+      
 
     Separador
     #t_2_2.titulo-segundo.color-acento-contenido
