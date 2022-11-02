fa206688ce24aab1b023c25dce78e718e90a5903
Edgar Mauricio Cortes <edgarm.cortes@misena.edu.co>
2022-11-02 11:54:45 -0500

    Ajustes

--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -7,6 +7,141 @@
         span 5
       h1 Instrumentos para la mejora continua en el guionaje
 
+    p.mb-5 En el servicio guiado es necesario manejar formatos como la bitácora y las novedades que permitan aplicar un proceso de mejora continua a quienes intervienen en el mismo, a continuación, se define su significado y se describen los elementos de la bitácora.
+
+    .fondo1.p-4.mb-5      
+      SlyderA(tipo="b")
+        .row.justify-content-center.mb-0.pt-4
+          .col-lg-4.col-10            
+            p.mb-lg-0.mb-3 La bitácora definida como el registro escrito de las actividades que se llevaron a cabo en el ejercicio de una determinada función y que contiene todos los eventos o sucesos que tuvieron lugar durante la realización de dicha actividad en un marco de tiempo (las fallas que se produjeron, los cambios realizados y los costos que se ocasionaron). 
+          .col-lg-1
+          .col-lg-6.col-12                  
+            img.mb-0(src="@/assets/curso/tema5/img01.png")  
+        .row.justify-content-center.mb-0.pt-4
+          .col-lg-4.col-10            
+            p.mb-lg-0.mb-3 La bitácora, permite que las empresas prestadoras de servicio (agencias de viajes) y al guía de turismo realicen un control detallado y estricto de todos los acontecimientos paso a paso presentados durante la realización de un recorrido o en general el desarrollo de eventos. 
+          .col-lg-1
+          .col-lg-6.col-12                  
+            img.mb-0(src="@/assets/curso/tema5/img02.png")  
+        .row.justify-content-center.mb-0.pt-4
+          .col-lg-4.col-10            
+            p.mb-lg-0.mb-3 El uso de bitácora permite el seguimiento y control de todas las actividades realizadas por el guía de parte de la agencia de viajes. El análisis resultado de esta herramienta contribuye en la toma de decisiones para la implementación de acciones de mejora en la prestación del servicio. (Servicio Nacional de Aprendizaje, 2018).
+          .col-lg-1
+          .col-lg-6.col-12                  
+            img.mb-0(src="@/assets/curso/tema5/img03.png")  
+        .row.justify-content-center.mb-0.pt-4
+          .col-lg-4.col-10            
+            p.mb-lg-0.mb-3 Es una herramienta que brinda información permanente a las empresas del sector turismo que ayuda para el mejoramiento continuo de la calidad del servicio ofrecido, y contribuye en mejorar la experiencia del turista.
+          .col-lg-1
+          .col-lg-6.col-12                  
+            img.mb-0(src="@/assets/curso/tema5/img04.png")  
+        .row.justify-content-center.mb-0.pt-4
+          .col-lg-4.col-10            
+            p.mb-lg-0.mb-3 Es importante que, al momento de redactar los acontecimientos y registros diarios, se realicen de forma clara, concreta y precisa, teniendo en cuenta que la bitácora puede ser un elemento de uso común. Por ello, su escritura y presentación deben tener calidad técnica y claridad para quien lee el documento.
+          .col-lg-1
+          .col-lg-6.col-12                  
+            img.mb-0(src="@/assets/curso/tema5/img05.png")  
+        .row.justify-content-center.mb-0.pt-4
+          .col-lg-4.col-10            
+            p.mb-lg-0.mb-3 En las bitácoras se contrasta, lo planeado con lo ejecutado con el fin de identificar las diferencias que serán tenidas en cuenta para realizar los ajustes pertinentes en el diseño del producto/servicio respecto a aspectos logísticos, económicos, locativos, administrativos, condiciones físicas de accesibilidad, entre otros.
+          .col-lg-1
+          .col-lg-6.col-12                  
+            img.mb-0(src="@/assets/curso/tema5/img06.png")  
+        .row.justify-content-center.mb-0.pt-4
+          .col-lg-4.col-10            
+            p.mb-4 Los elementos que componen la bitácora son:
+            ul.lista-ul--color.mb-lg-0.mb-3
+              li.mb-1
+                i.fas.fa-location-arrow
+                .p-0 Los elementos que componen la bitácora son:
+              li.mb-1
+                i.fas.fa-location-arrow
+                .p-0 Fecha de realización del tour o recorrido. 
+              li.mb-1
+                i.fas.fa-location-arrow
+                .p-0 Hora de salida y regreso. 
+              li.mb-1
+                i.fas.fa-location-arrow
+                .p-0 Sitio de origen y sitio donde finaliza el tour o recorrido. 
+              li.mb-1
+                i.fas.fa-location-arrow
+                .p-0 Nombre de la persona responsable del manejo y conducción del tour. 
+              li.mb-1
+                i.fas.fa-location-arrow
+                .p-0 Nombre de la empresa que opera el tour. 
+              li.mb-1
+                i.fas.fa-location-arrow
+                .p-0 Número de turistas por nacionalidad. 
+              li.mb-1
+                i.fas.fa-location-arrow
+                .p-0 Paralelo:  evaluación de lo ejecutado frente a los planeado
+              li.mb-0
+                i.fas.fa-location-arrow
+                .p-0 Recomendaciones o acciones de mejora para que en próximos servicios no se replique situaciones problema.
+          .col-lg-1
+          .col-lg-6.col-12                  
+            img.mb-0(src="@/assets/curso/tema5/img07.png")  
+
+    p.mb-5 Con base en la información registrada en la bitácora, se puede realizar un ajuste en la presentación del servicio; después de diligenciar una bitácora por parte del guía de turismo y entregarla a la empresa contratante, se da inicio al proceso del análisis y perfeccionamiento de la misma; partiendo de esto, se realizan ajustes del servicio en un recorrido en relación con cronogramas, itinerarios, tiempos, equipos, recursos, entre otros aspectos; en caso de reporte de imprevistos en la bitácora, es importante tener en cuenta las recomendaciones de los guías de turismo y turistas, ver tabla 2. 
+        
+    .fondo2.mb-5 
+      .row.justify-content-center.align-items-center.mb-0.pt-4
+        .col-lg-3.col-8.px-5
+          img.mb-lg-0.mb-3(src="@/assets/curso/tema5/img08.png")  
+        .col-lg-8.col-12.pt-lg-5                         
+          .row.justify-content-between.align-items-center
+            .col.mb-3.mb-sm-0
+              h3.mb-4 Tabla 2
+              p.mb-lg-0.mb-3 A continuación le invitamos a profundizar sobre el reporte de imprevistos en la bitácora teniendo encuentra las recomendaciones que encontrara en la siguiente tabla.
+            .col-sm-auto
+                    a.boton.color-acento-botones.texto-blanco(:href="obtenerLink('downloads/prueba.pdf')" target="_blank")
+                      span Descargar
+                      i.fas.fa-file-download
+
+    .row.justify-content-center.align-items-center.mb-5 
+      .col-lg-10.col-12
+        .cajon.color-primario.p-4.mb-0
+            p.mb-0 La Real Academia de la Lengua, define el término novedad como la acción de informar sobre un suceso reciente del que se da noticia.
+
+    .row.justify-content-center.py-3.mb-5       
+      .col-lg-6.col-12        
+          p.mb-4 Debido a que el guía entre sus funciones tiene la de asistir a pasajeros y la conducción de estos, debe informarse al contratante, los detalles de un suceso relacionado con la salud de un usuario, el incumplimiento de un servicio contratado y cualquier otro asunto que amerite una detallada descripción de lo sucedido. Adicionalmente, se incluye al informe, una situación que se define como incidente o en el caso de accidente y cómo se procede ante estas situaciones. 
+          p.mb-lg-0.mb-3 Todas las personas en una actividad turística deben contar con un seguro o póliza, por tanto, se debe evaluar la situación en lo posible con de Atención Pre-hospitalaria, APH, cuando se presenta un accidente, así como hacer el trámite para la cobertura de la póliza por medio de las líneas o canales establecidos, y seguir el conducto de atención apoyados en el equipo de trabajo.          
+      .col-lg-6.col-8
+        img.mb-0(src="@/assets/curso/tema5/img09.png")
+
+    .row.justify-content-center.align-items-center.mb-5       
+      .col-lg-6.col-8
+        img.mb-lg-0.mb-3(src="@/assets/curso/tema5/img10.png")
+      .col-lg-6.col-12        
+          p.mb-4 Elementos que debe contener el formato de novedades:
+          ul.lista-ul--color.mb-0
+            li.mb-2
+              i.fas.fa-location-arrow
+              .p-0 Nombre de la empresa que opera el tour. 
+            li.mb-2
+              i.fas.fa-location-arrow
+              .p-0 Detalles del servicio: fecha, # de turistas por nacionalidad. 
+            li.mb-2
+              i.fas.fa-location-arrow
+              .p-0 Identificación del equipo o situación (médica, emergencia, incumplimiento)
+            li.mb-2
+              i.fas.fa-location-arrow
+              .p-0 Una imagen de los sucedidos
+            li.mb-2
+              i.fas.fa-location-arrow
+              .p-0 Posibles riesgos de no atender la novedad
+            li.mb-0
+              i.fas.fa-location-arrow
+              .p-0 Sugerencia de cómo tratar la novedad
+          
+    p.mb-4.text-center A continuación, se muestra un ejemplo del formato de novedades, ver figura 4.
+
+    .row.justify-content-center.align-items-center.mb-0       
+      .col-lg-8.col-12
+        .titulo-sexto.color-acento-contenido(data-aos="fade-left")
+          p.titulo <b>Figura 4</b> <i>Formato de novedades</i> 
+        img.mb-0(src="@/assets/curso/tema5/img11.png")
 </template>
 
 <script>
